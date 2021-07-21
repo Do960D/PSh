2cf443d9b1b66b006c5b26d03b3dce46db137368
Do960D <87614970+Do960D@users.noreply.github.com>
2021-07-21 17:32:56 +0300

    Update local-clean-up.ps1

--- local-clean-up.ps1
+++ local-clean-up.ps1
@@ -1,7 +1,7 @@
 ﻿$DayX = (Get-Date).addDays(-200)   #Set date of rancid
 Get-CimInstance Win32_userprofile |#get all profiles
 ? lastusetime 		          |#get readeble date format
-select lastusetime, localpath 	  |#choose the right one 
+select lastusetime, localpath 	  |#choose necessary info 
 where localpath -like 'C:\Users\*'|#select non special profiles
 Where lastusetime -lt $DayX  	  |#select rancided profiles
 Sort-Object -Property lastusetime  #sort by time
