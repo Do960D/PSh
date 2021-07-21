a5649689306e91f7dec5754442ad66f58ca0c1b0
Do960D <87614970+Do960D@users.noreply.github.com>
2021-07-21 17:16:04 +0300

    Its ALIVE. v1.0
    Seeking for abandoned ADAccaunts, disable them, and move from prod OU to special OU for disabled users

---
+++ Seek&Entomb.ps1
@@ -0,0 +1,12 @@
+﻿#Before we start catch this
+$Entombment = 'OU=moved,DC=Dom,DC=local'
+$HereWeHunt = 'OU=Регионы,DC=Dom,DC=local'
+$DecayDate = (Get-Date).addDays(-90) 
+$PerceiveAsANewBorn = (Get-Date).addDays(-30)
+
+#Fence the Hunting Grounds
+$THG = Get-Aduser -f {(LastLogonDate -lt $DecayDate) -or -not (lastlogontimestamp -like '*') -and -not (iscriticalsystemobject -eq $true) -and (Created -lt $PerceiveAsANewBorn)} -Property * -SearchBase $HereWeHunt |
+    Where {$_.DistinguishedName -notmatch 'Секретарь'}      |
+    Where {$_.DistinguishedName -notmatch 'Системный*'}     |
+    Select name
+Search-ADAccount $THG -UsersOnly -SearchBase $HereWeHunt | move-ADObject -TargetPath $Entombment -WhatIf
