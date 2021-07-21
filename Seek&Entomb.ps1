c4a65b849ff412c114b5bb0215eb57a01dbdfea9
Do960D <87614970+Do960D@users.noreply.github.com>
2021-07-21 17:25:35 +0300

    Update Seek&Entomb.ps1

--- Seek&Entomb.ps1
+++ Seek&Entomb.ps1
@@ -1,12 +1,19 @@
-﻿#Before we start catch this
-$Entombment = 'OU=RIP,DC=Dom,DC=local'
-$HereWeHunt = 'OU=PROD,DC=Dom,DC=local'
-$DecayDate = (Get-Date).addDays(-90) 
-$PerceiveAsANewBorn = (Get-Date).addDays(-30)
-
+#Before we start catch this
+    $Entombment = 'DC=RIP,DC=local'
+    $HereWeHunt = 'DC=DOM,DC=local'
+    $DecayDate = (Get-Date).addDays(-90) 
+    $PerceiveAsANewBorn = (Get-Date).addDays(-30)
+    
+#Burie the dead
+    Search-ADAccount -AccountDisabled -UsersOnly -SearchBase $HereWeHunt | Move-ADObject -TargetPath $Entombment
+       
 #Fence the Hunting Grounds
-$THG = Get-Aduser -f {(LastLogonDate -lt $DecayDate) -or -not (lastlogontimestamp -like '*') -and -not (iscriticalsystemobject -eq $true) -and (Created -lt $PerceiveAsANewBorn)} -Property * -SearchBase $HereWeHunt |
-    Where {$_.DistinguishedName -notmatch 'Секретарь'}      |
-    Where {$_.DistinguishedName -notmatch 'Системный*'}     |
-    Select name
-Search-ADAccount $THG -UsersOnly -SearchBase $HereWeHunt | move-ADObject -TargetPath $Entombment -WhatIf
+    Get-Aduser -f {(LastLogonDate -lt $DecayDate) -or -not (lastlogontimestamp -like '*') -and -not (iscriticalsystemobject -eq $true) -and (Created -lt $PerceiveAsANewBorn)} -Property * -SearchBase $HereWeHunt |
+        Where DistinguishedName -notmatch 'Секретарь'      |
+        Where DistinguishedName -notmatch 'Системный*'      |
+
+#Exile The Undead
+        Move-ADObject -TargetPath $Entombment 
+                     
+#Rest In Peace
+     Get-ADUser -Filter * -SearchBase $Entombment | Disable-ADAccount
